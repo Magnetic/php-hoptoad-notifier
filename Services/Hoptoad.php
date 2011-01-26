a420899ca587ba8166708dc28c98cf43510fe465
Robert Rotarius <robert_rotarius@magnetic.is>
2011-01-26 11:34:44 -0500

    Added support for handling fatal errors

--- Services/Hoptoad.php
+++ Services/Hoptoad.php
@@ -82,6 +82,7 @@ class Services_Hoptoad
 	 **/
 	public function installNotifierHandlers()
 	{
+	  register_shutdown_function(array($this, "fatalErrorHandler"));
 		set_error_handler(array($this, "errorHandler"));
 		set_exception_handler(array($this, "exceptionHandler"));		
 	}
@@ -146,6 +147,18 @@ class Services_Hoptoad
 	{
 		$this->notify(get_class($exception), $exception->getMessage(), $exception->getFile(), $exception->getLine(), $exception->getTrace());
 	}
+	
+  /**
+   * Handle fatal errors
+   *
+   * @return void
+   * @author Robert Rotarius
+   */
+  public function fatalErrorHandler() 
+  {
+    $error = error_get_last(); 
+    $this->notify($error['type'], $error['message'], $error['file'], $error['line'], debug_backtrace());
+  }	
 
 	/**
 	 * Set the values to be used for the next notice sent to Hoptoad
